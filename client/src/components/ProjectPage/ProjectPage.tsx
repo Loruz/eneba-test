f8c794b6a51fcc37bc12da9d6d654efe79253839
Preiksas <pvytautas@whatagraph.com>
2021-03-29 17:30:26 +0300

    Testimonials block

--- client/src/components/ProjectPage/ProjectPage.tsx
+++ client/src/components/ProjectPage/ProjectPage.tsx
@@ -2,12 +2,16 @@ import React, { FC } from 'react'
 import ProjectCard from '../ProjectCard/ProjectCard'
 import ProjectSidebar from '../ProjectSidebar/ProjectSidebar'
 import styles from './ProjectPage.module.css'
+import TestimonialList from '../TestimonialList/TestimonialList'
 
 const ProjectPage: FC = () => {
   return (
-    <main className={styles.page}>
-     <ProjectCard/>
-     <ProjectSidebar/>
+    <main className={styles.main}>
+      <section className={styles.page}>
+        <ProjectCard/>
+        <ProjectSidebar/>
+      </section>
+      <TestimonialList/>
     </main>
   )
 }
--- client/src/components/TestimonialList/TestimonialList.tsx
+++ client/src/components/TestimonialList/TestimonialList.tsx
@@ -1,8 +1,20 @@
 import React, { FC } from 'react'
+import styles from './Testimonial.module.css'
 
 const TestimonialList: FC = () => {
+  let testimonials = [1, 2, 3].map(t => {
+    return (
+      <div className={styles.testimonial} key={t}>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus.
+      </div>
+    )
+  })
   return (
-    <div>Test</div>
+    <div className={styles.testimonials}>
+      {testimonials}
+    </div>
   )
 }
 export default TestimonialList
